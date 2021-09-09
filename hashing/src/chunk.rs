206ab921fef94668b1f612f6d3a699cc9d928933
Rafał Chabowski <rafal@casperlabs.io>
2021-09-09 11:14:25 +0200

    Add tests for `ChunkWithProof`

--- hashing/src/chunk.rs
+++ hashing/src/chunk.rs
@@ -10,7 +10,12 @@ pub struct ChunkWithProof {
 }
 
 impl ChunkWithProof {
+    #[cfg(test)]
+    pub const CHUNK_SIZE: usize = 1;
+
+    #[cfg(not(test))]
     pub const CHUNK_SIZE: usize = 1_048_576; // 2^20
+
     pub fn new(data: &[u8], index: u64) -> Result<Self, MerkleConstructionError> {
         if data.len() < Self::CHUNK_SIZE * (index as usize) {
             return Err(todo!());
@@ -28,5 +33,61 @@ impl ChunkWithProof {
 #[cfg(test)]
 mod test {
     // TODO: test empty chunk
-    // Make proptests to make sure that ChunkWithProof agrees with hash_merkle_tree of the chunked data
+    // Make proptests to make sure that ChunkWithProof agrees with hash_merkle_tree of the chunked
+    // data
+
+    use rand::Rng;
+    use std::convert::TryInto;
+
+    use crate::{
+        chunk::ChunkWithProof,
+        primitives::{blake2b_hash, hash_merkle_tree, IndexedMerkleProof},
+    };
+
+    fn prepare_bytes(length: usize) -> Vec<u8> {
+        let mut rng = rand::thread_rng();
+
+        let mut v = Vec::with_capacity(length);
+        (0..length).into_iter().for_each(|_| v.push(rng.gen()));
+        v
+    }
+
+    #[test]
+    fn first_hash_in_proof_matches_data_hash() {
+        let large_data_size = (ChunkWithProof::CHUNK_SIZE * 1024) as usize;
+        let large_data = prepare_bytes(large_data_size);
+
+        let number_of_chunks: u64 = large_data
+            .chunks(ChunkWithProof::CHUNK_SIZE)
+            .len()
+            .try_into()
+            .unwrap();
+
+        (0..number_of_chunks)
+            .into_iter()
+            .map(|chunk_index| ChunkWithProof::new(large_data.as_slice(), chunk_index).unwrap())
+            .map(|cwp| (cwp.chunk, cwp.proof.merkle_proof()[0].clone()))
+            .map(|(chunk, first_hash)| (blake2b_hash(chunk), first_hash))
+            .for_each(|(chunk_hash, first_hash)| assert_eq!(chunk_hash, first_hash));
+    }
+
+    #[test]
+    fn validate_chunks_against_hash_merkle_tree() {
+        let data = prepare_bytes(ChunkWithProof::CHUNK_SIZE * 2);
+        let expected_root =
+            hash_merkle_tree(data.chunks(ChunkWithProof::CHUNK_SIZE).map(blake2b_hash));
+
+        // Calculate proof with `ChunkWithProof`
+        let ChunkWithProof {
+            proof: proof_0,
+            chunk: _,
+        } = ChunkWithProof::new(data.as_slice(), 0).unwrap();
+        let ChunkWithProof {
+            proof: proof_1,
+            chunk: _,
+        } = ChunkWithProof::new(data.as_slice(), 1).unwrap();
+
+        assert_eq!(proof_0.root_hash(), expected_root);
+        assert_eq!(proof_1.root_hash(), expected_root);
+    }
 }
