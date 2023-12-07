67bfaa2180027caf0cd8f8c8e6e4bace8c19b3c1
Jacek Malec <145967538+jacek-casper@users.noreply.github.com>
2023-12-07 14:32:12 +0000

    Fix compilation errors

--- node/src/components/storage/tests.rs
+++ node/src/components/storage/tests.rs
@@ -24,11 +24,11 @@ use casper_types::{
     generate_ed25519_keypair,
     system::auction::UnbondingPurse,
     testing::TestRng,
-    AccessRights, AvailableBlockRange, Block, BlockHash, BlockHashAndHeight, BlockHeader,
-    BlockSignatures, BlockV2, Chainspec, ChainspecRawBytes, Deploy, DeployApprovalsHash,
-    DeployHash, Digest, EraId, FinalitySignature, ProtocolVersion, PublicKey, SecretKey,
-    SignedBlock, SignedBlockHeader, TestBlockBuilder, TestBlockV1Builder, TimeDiff, Transaction,
-    TransactionApprovalsHash, TransactionHash, Transfer, URef, U512,
+    AccessRights, AvailableBlockRange, Block, BlockHash, BlockHeader, BlockSignatures, BlockV2,
+    Chainspec, ChainspecRawBytes, Deploy, DeployApprovalsHash, DeployHash, Digest, EraId,
+    ExecutionInfo, FinalitySignature, Key, ProtocolVersion, PublicKey, SecretKey, SignedBlock,
+    SignedBlockHeader, TestBlockBuilder, TestBlockV1Builder, TimeDiff, Transaction,
+    TransactionApprovalsHash, TransactionHash, TransactionV1Hash, Transfer, URef, U512,
 };
 use tempfile::tempdir;
 
@@ -46,8 +46,8 @@ use crate::{
     },
     testing::{ComponentHarness, UnitTestEvent},
     types::{
-        sync_leap_validation_metadata::SyncLeapValidationMetaData, ApprovalsHashes, ExecutionInfo,
-        LegacyDeploy, SyncLeapIdentifier, TransactionWithFinalizedApprovals,
+        sync_leap_validation_metadata::SyncLeapValidationMetaData, ApprovalsHashes, LegacyDeploy,
+        SyncLeapIdentifier, TransactionWithFinalizedApprovals,
     },
     utils::{Loadable, WithDir},
 };
